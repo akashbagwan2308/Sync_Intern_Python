e80c79cffb76a2c42ad3643ed01bb967029a8261
Akash Bagwan <110125088+akashbagwan2308@users.noreply.github.com>
2023-07-05 23:24:20 +0530

    Add files via upload

---
+++ task 4/bot.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbotmodel.h5')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(message):
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    return res
+
+# Streamlit web app
+def main():
+    st.title("Chatbot App")
+    st.subheader("A chat bot designed by Akash Bagwan")
+
+    if 'conversation' not in st.session_state:
+        st.session_state.conversation = []
+
+    user_input = st.text_input("User Input")
+    if st.button("Chat"):
+        bot_response = chatbot_response(user_input)
+        st.session_state.conversation.append({"user_input": user_input, "bot_response": bot_response})
+
+    if st.button("Clear Chat"):
+        st.session_state.conversation = []
+
+    for conv in st.session_state.conversation:
+        st.text(f"You: {conv['user_input']}")
+        st.text(f"Bot: {conv['bot_response']}")
+
+if __name__ == '__main__':
+    main()
